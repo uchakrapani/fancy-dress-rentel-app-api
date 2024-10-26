d879f2cedbdcbd3d23849e423ca598f97c032b8d
chakrapani <chakrapani24@gmail.com>
2024-10-26 17:23:51 +0530

    initiL COMMIT 1234

--- config/winston.js
+++ config/winston.js
@@ -1,24 +1,16 @@
-// config/winston.js
 const { createLogger, format, transports } = require('winston');
-const path = require('path');
-const fs = require('fs');
-
-// Define the logs directory
-const logsDir = path.join(__dirname, '../logs');
-
-// Check if the logs directory exists, if not create it
-if (!fs.existsSync(logsDir)) {
-    fs.mkdirSync(logsDir, { recursive: true }); // recursive: true creates nested directories
-}
 
 const logger = createLogger({
+    level: 'info',
     format: format.combine(
         format.timestamp(),
         format.json()
     ),
     transports: [
+        // Log to the console
         new transports.Console(),
-        new transports.File({ filename: path.join(logsDir, 'error.log') })
+        // Optionally, you can add a cloud-based logging service
+        // e.g., new transports.Http({ url: 'https://example.com/logs' })
     ]
 });
 
