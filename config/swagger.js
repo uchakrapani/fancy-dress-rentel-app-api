2629228909e5c82157cd0ed60acca444a6d8dac7
chakrapani <chakrapani24@gmail.com>
2024-10-27 10:13:50 +0530

    updated swagger

--- config/swagger.js
+++ config/swagger.js
@@ -1,24 +1,27 @@
 // config/swagger.js
-const swaggerJSDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 const swaggerDefinition = {
-    openapi: '3.0.0',
-    info: {
-        title: 'Fancy Dress Rental API',
-        version: '1.0.0',
-        description: 'API documentation for the Fancy Dress Rental application',
+  openapi: "3.0.0",
+  info: {
+    title: "Fancy Dress Rental API",
+    version: "1.0.0",
+    description: "API documentation for the Fancy Dress Rental application",
+  },
+  servers: [
+    {
+      url: "http://localhost:3000/api",
     },
-    servers: [
-        {
-            url: 'http://localhost:3000/api',
-        },
-    ],
+    {
+      url: "https://fancy-dress-rentel-app-api.vercel.app/api",
+    },
+  ],
 };
 
 const options = {
-    swaggerDefinition,
-    apis: ['./routes/*.js', './controllers/*.js'], // Path to the API docs
+  swaggerDefinition,
+  apis: ["./routes/*.js", "./controllers/*.js"], // Path to the API docs
 };
 
 const swaggerSpec = swaggerJSDoc(options);
